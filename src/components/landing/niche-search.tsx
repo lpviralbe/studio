8e555287b54692c3a7afd5edce03fe14791ca2f7
Diogo Santana <diogoaugustobenides@gmail.com>
2025-10-20 17:55:58 +0000

    anime as img coloque animação (_for element img_)

--- src/components/landing/niche-search.tsx
+++ src/components/landing/niche-search.tsx
@@ -15,34 +15,34 @@ export function NicheSearch() {
             <Search className="h-6 w-6 md:mr-3" />
             Encontrar os vídeos mais virais do meu nicho
         </Button>
-        <div className="mt-4 flex justify-center items-center gap-4 animate-fade-in-up [animation-delay:300ms]">
+        <div className="mt-4 flex justify-center items-center gap-4">
             <Image
                 src="https://i.imgur.com/NNzOliK.png"
                 alt="Powered by Gemini"
                 width={120}
                 height={40}
-                className="h-auto"
+                className="h-auto animate-fade-in-up [animation-delay:300ms]"
             />
             <Image
                 src="https://i.imgur.com/deUD5R5.png"
                 alt="Mais um logo"
                 width={120}
                 height={40}
-                className="h-auto"
+                className="h-auto animate-fade-in-up [animation-delay:400ms]"
             />
             <Image
                 src="https://i.imgur.com/UuRc8mX.png"
                 alt="Mais um logo"
                 width={120}
                 height={40}
-                className="h-auto"
+                className="h-auto animate-fade-in-up [animation-delay:500ms]"
             />
             <Image
                 src="https://i.imgur.com/7P9pUs4.png"
                 alt="Mais um logo"
                 width={120}
                 height={40}
-                className="h-auto"
+                className="h-auto animate-fade-in-up [animation-delay:600ms]"
             />
         </div>
       </div>
