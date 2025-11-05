4048ae4959a4d463ff5a1423eb252c9ae06786b2
Diogo Santana <diogoaugustobenides@gmail.com>
2025-11-05 01:02:47 +0000

    adicione a aba : Como usar em 3 passos O processo é tão simples que pare

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,8 +29,8 @@ function HomePageContent() {
 
       <main className="flex-1">
         <Hero />
-        <ProblemSolution />
         <HowItWorks />
+        <ProblemSolution />
         <SocialProof />
         <Testimonials />
         <Pricing />
